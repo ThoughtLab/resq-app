426d172bf6e637c05839587c9c53c63307ba1b86
maysam.tayyeb <Tnz6PpuL3QmGrmDJtJ5H>
2019-06-12 11:16:45 +1000

    Polished

--- screens/HelperScreen.js
+++ screens/HelperScreen.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
 
 export default class HelperScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    header: null,
   };
 
   constructor() {
